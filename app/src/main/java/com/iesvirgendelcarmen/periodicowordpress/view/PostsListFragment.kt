8a6236fa78edf6fa57791c5e2005282d7a4b6ba3
Álvaro García <alvvdc@gmail.com>
2020-05-11 08:39:47 +0200

    Vista de advertencia para cuando no hay marcadores guardados. 	modified:   app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/view/PostsListFragment.kt 	new file:   app/src/main/res/drawable/ic_bookmark_border_black_48dp.xml 	modified:   app/src/main/res/layout/fragment_posts_list.xml 	new file:   app/src/main/res/layout/view_there_are_no_bookmarks.xml

--- app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/view/PostsListFragment.kt
+++ app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/view/PostsListFragment.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -16,10 +18,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.iesvirgendelcarmen.periodicowordpress.BookmarkPostListener
-import com.iesvirgendelcarmen.periodicowordpress.MainActivity
-import com.iesvirgendelcarmen.periodicowordpress.R
-import com.iesvirgendelcarmen.periodicowordpress.SharePostListener
+import com.iesvirgendelcarmen.periodicowordpress.*
 import com.iesvirgendelcarmen.periodicowordpress.config.Endpoint
 import com.iesvirgendelcarmen.periodicowordpress.model.Resource
 import com.iesvirgendelcarmen.periodicowordpress.model.businessObject.MenuCategory
@@ -28,6 +27,7 @@ import com.iesvirgendelcarmen.periodicowordpress.model.room.Bookmark
 import com.iesvirgendelcarmen.periodicowordpress.model.room.BookmarkList
 import com.iesvirgendelcarmen.periodicowordpress.viewmodel.BookmarkViewModel
 import com.iesvirgendelcarmen.periodicowordpress.viewmodel.PostBoViewModel
+import java.util.zip.Inflater
 
 
 class PostsListFragment :   Fragment(),
@@ -52,6 +52,7 @@ class PostsListFragment :   Fragment(),
 
     lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var noBookmarksView: ConstraintLayout
 
     private lateinit var postsListRecyclerViewAdapter: PostsListRecyclerViewAdapter
     private lateinit var postsListRecyclerViewOnScrollListener: PostsListRecyclerViewOnScrollListener
@@ -109,6 +110,9 @@ class PostsListFragment :   Fragment(),
 
         bottomNavigation = view.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigation.setOnNavigationItemSelectedListener(this)
+
+        noBookmarksView = view.findViewById(R.id.noBookmarksView)
+        noBookmarksView.visibility = View.GONE
     }
 
     override fun onStart() {
@@ -140,7 +144,12 @@ class PostsListFragment :   Fragment(),
                 val bookmarkLiveData = bookmarkViewModel.getAll()
 
                 bookmarkLiveData.observe(viewLifecycleOwner, Observer {
-                    postViewModel.getPosts(paginationStatus.page, paginationStatus.category, Bookmark.convertListToArray(it))
+                    if (it.isEmpty()) {
+                        noBookmarksView.visibility = View.VISIBLE
+                        swipeRefresh.isRefreshing = false
+                    } else {
+                        postViewModel.getPosts(paginationStatus.page, paginationStatus.category, Bookmark.convertListToArray(it))
+                    }
                 })
 
                 bottomNavigation.checkItem(R.id.bookmark)
