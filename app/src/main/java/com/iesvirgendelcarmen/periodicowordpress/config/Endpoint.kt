dcadc1ae97f9ac9c300b6e2b5ba88d36a5211ff7
Álvaro García <alvvdc@gmail.com>
2020-05-15 09:13:37 +0200

    Encapsulación y refactorización de Endpoints. Ajustados pequeños detalles en el layout del detalle de un Post. 	modified:   app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/config/Endpoint.kt 	modified:   app/src/main/res/layout/fragment_post_detail.xml

--- app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/config/Endpoint.kt
+++ app/src/main/java/com/iesvirgendelcarmen/periodicowordpress/config/Endpoint.kt
@@ -3,35 +3,36 @@ package com.iesvirgendelcarmen.periodicowordpress.config
 class Endpoint {
     companion object {
 
-        val BASE_URL = "http://192.168.1.8:8080/index.php?rest_route=/wp/v2/"
+        private const val BASE_URL = "http://192.168.1.8:8080/index.php?rest_route=/wp/v2/"
 
-        //val BASE_URL = "https://time.com/index.php?rest_route=/wp/v2/"
-        //val BASE_URL = "https://newyork.cbslocal.com/index.php?rest_route=/wp/v2/"
-        //val BASE_URL = "https://chinadigitaltimes.net/index.php?rest_route=/wp/v2/"
+        //private const val BASE_URL = "https://time.com/index.php?rest_route=/wp/v2/"
+        //private const val BASE_URL = "https://newyork.cbslocal.com/index.php?rest_route=/wp/v2/"
+        //private const val BASE_URL = "https://chinadigitaltimes.net/index.php?rest_route=/wp/v2/"
 
-        val POSTS = "posts"
-        val POSTS_URL = "$BASE_URL$POSTS/"
+        private const val POSTS = "posts"
+        const val POSTS_URL = "$BASE_URL$POSTS/"
 
-        val CATEGORIES = "categories"
-        val CATEGORIES_URL = "$BASE_URL$CATEGORIES/"
+        private const val CATEGORIES = "categories"
+        const val CATEGORIES_URL = "$BASE_URL$CATEGORIES/"
 
-        val MEDIA = "media"
-        val MEDIA_URL = "$BASE_URL$MEDIA/"
+        private const val MEDIA = "media"
+        const val MEDIA_URL = "$BASE_URL$MEDIA/"
 
-        val USERS = "users"
-        val USERS_URL = "$BASE_URL$USERS/"
+        private const val USERS = "users"
+        const val USERS_URL = "$BASE_URL$USERS/"
 
-        val PAGE = "&page="
-        val PER_PAGE = "&per_page="
-        val DEFAULT_PER_PAGE = 10
+        const val PAGE = "&page="
+        const val PER_PAGE = "&per_page="
+        const val DEFAULT_PER_PAGE = 10
 
-        val CATEGORIES_OPTION = "&categories="
-        val INCLUDE = "&include="
-        val ORDER_BY = "&orderby="
-        val ORDER = "&order="
-        val DESC = "desc"
-        val COUNT = "count"
-        val ORDER_BY_COUNT = "$ORDER_BY$COUNT"
-        val ORDER_DESC = "$ORDER$DESC"
+        const val CATEGORIES_OPTION = "&categories="
+        const val INCLUDE = "&include="
+
+        private const val ORDER_BY = "&orderby="
+        private const val ORDER = "&order="
+        private const val DESC = "desc"
+        private const val COUNT = "count"
+        const val ORDER_BY_COUNT = "$ORDER_BY$COUNT"
+        const val ORDER_DESC = "$ORDER$DESC"
     }
 }
